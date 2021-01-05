14f250c9f14a150b263c5ea97e1bdf0c72c88b39
Obinna Okafor <okaforobinna7@gmail.com>
2021-01-05 18:06:34 +0100

    commit for exercise 2.6

---
+++ part2/the-phonebook/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Person = ({person}) => <li>{person.name}</li>
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+      console.log(event.target.value);
+      setNewName(event.target.value);
+  }
+
+  const addName = (event) => {
+      event.preventDefault();
+      const nameObject = {
+          name: newName
+      }
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit= {addName}>
+        <div>
+          name: <input value={newName} onChange= {handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+       <ul>
+           {persons.map(person =>
+              <Person key= {person.name} person= {person} />
+           )}
+       </ul>
+    </div>
+  )
+}
+
+export default App
