324422e7748611b774fa1469c0c3c46885462a9b
Obinna Okafor <okaforobinna7@gmail.com>
2021-02-02 00:00:50 +0100

    Commit for exercise 4.4

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes =(blogs) => {
+  let countLikes= 0
+  blogs.forEach(blog => {
+    countLikes += blog.likes
+  })
+  return countLikes
+
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
