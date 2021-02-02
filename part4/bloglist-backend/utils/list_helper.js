1cc1894ab24d09209ff7378d2f259767a45a6d62
Obinna Okafor <okaforobinna7@gmail.com>
2021-02-02 02:45:12 +0100

    Commit for exercise 4.6

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,5 +1,7 @@
 //onst blog = require("../models/blog")
 
+//const { delete } = require("../app")
+
 const dummy = (blogs) => {
   // ...
   return 1
@@ -39,6 +41,57 @@ const favoriteBlog =(blogs) => {
   return maxLikesBlog
 }
 
-module.exports = {
-  dummy, totalLikes, favoriteBlog
+//helper function for mostBlogs function
+const getMaxBlogsAuthor = (blogObjectsArr) => {
+  let maxBlogsAuthor = {
+    author: '',
+    title: '',
+    blogs: 0
+  }
+
+  blogObjectsArr.forEach((obj) => {
+    if(obj.blogs > maxBlogsAuthor.blogs){
+      maxBlogsAuthor= { ...obj }
+    }
+  })
+  return maxBlogsAuthor
 }
+
+const mostBlogs = (arrBlogs) => {
+  const arrBlogsCopy= [...arrBlogs]
+
+  arrBlogsCopy.forEach((obj) => {
+    obj.blogs= 0
+    obj.checked= false
+    delete obj.likes
+    delete obj.url
+    delete obj._id
+    delete obj.__v
+  })
+
+  arrBlogsCopy.forEach((obj) => {
+    if(obj.checked === false){
+      obj.blogs = obj.blogs + 1
+      obj.checked = true
+      arrBlogsCopy.forEach((obj2) => {
+        if(obj2.checked === false && obj2.author === obj.author){
+          obj.blogs = obj.blogs + 1
+          obj2.checked= true
+        }
+      })
+    }
+  })
+  //filter out objects whose blog key valuea are o
+  //console.log('arrBlogsCopy is ', arrBlogsCopy)
+  const filteredArrBlogs= arrBlogsCopy.filter((obj => obj.blogs !== 0))
+  let maxBlogsObject = getMaxBlogsAuthor(filteredArrBlogs)
+
+  delete maxBlogsObject.title
+  delete maxBlogsObject.checked
+  return maxBlogsObject
+}
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog, mostBlogs
+}
+
