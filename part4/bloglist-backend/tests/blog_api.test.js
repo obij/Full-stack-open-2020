994b60211b5f66c0ecde842e2b50be43779ec2b9
Obinna Okafor <okaforobinna7@gmail.com>
2021-02-05 22:55:08 +0100

    Commit for exercise 4.8

---
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
+
+//jest -t 'blog_api.test'
+//npm test -- tests/blog_api.test.js
