b4e6dfb03826a2098c5946530d5f9133166e7ee3
Obinna Okafor <okaforobinna7@gmail.com>
2021-02-08 18:52:47 +0100

    Commit for exercise 4.19

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const { response } = require('express')
 const Blog= require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 //const helper= require('../tests/test_helper')
 
 
@@ -10,16 +11,30 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.post('/', async (request, response) => {
   //const blog = new Blog(request.body)
   const body= request.body
 
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   //const user = await User.findById(body.userId)
   //const usersDb= await helper.usersInDb()
   //const user= usersDb[0]
-  const users= await User.find({})
-  const user= users[0]
+  //const users= await User.find({})
+  //const user= users[0]
 
   if(!body.likes){
     body.likes= 0
