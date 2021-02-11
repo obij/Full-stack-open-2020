2d0ccdd2a42ec6a69cd5e57241c12eb0085f28ee
Obinna Okafor <okaforobinna7@gmail.com>
2021-02-11 06:34:28 +0100

    Commit for exercise 4.21

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,5 +1,5 @@
 const blogsRouter = require('express').Router()
-const { response } = require('express')
+const { response, request } = require('express')
 const Blog= require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
@@ -19,6 +19,7 @@ blogsRouter.post('/', async (request, response) => {
 
   //const token = getTokenFrom(request)
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  console.log('decodedToken is ', decodedToken)
   if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
@@ -55,9 +56,38 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog)
 })
 
+
+
 blogsRouter.delete('/:id', async(request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  const blog= await Blog.findById(request.params.id)
+  const { id } = request.params
+  //console.log(typeof blog.user.id.toString('ascii'))
+  //let blogAscii = blog.user.id.toString('ascii')
+  //let blogConverted= String.fromCharCode(blogAscii)
+  //console.log('blogConverted is ', blogConverted)
+  //console.log(typeof blogConverted)
+
+  //if(decodedToken.id.toString() === JSON.stringify(blog.user.id)){
+  //await Blog.findByIdAndRemove(request.params.id)
+  //response.status(204).end()
+  //}else{
+  //response.status(400).end()
+  //}
+  const user = await User.findById(decodedToken.id)
+
+
+  if (blog.user.toString() === user._id.toString()) {
+    console.log('blog.user.toString() is ', blog.user.toString)
+    console.log('user._id.toString() is', user._id.toString())
+    await Blog.findByIdAndRemove(id)
+    response.status(204).end()
+  } else {
+    response
+      .status(401)
+      .json({ error: 'You are not authorized to delete this blog' })
+  }
 })
 
 blogsRouter.put('/:id', async(request, response) => {
