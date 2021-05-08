25587f36f3e0cb7104e7b8fa65c65ed57801af0e
Obinna Okafor <okaforobinna7@gmail.com>
2021-05-08 09:44:44 +0100

    Commit for exercise 6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -3,15 +3,17 @@ import {  useDispatch } from 'react-redux'
 import {  createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 import { removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  const addAnecdote= (event) => {
+  const addAnecdote= async (event) => {
     event.preventDefault()
     const anecdote= event.target.anecdote.value
     event.target.anecdote.value= ''
-    dispatch(createAnecdote(anecdote))
-    dispatch(setNotification(`Created a new anecdote: ${anecdote}`))
+    const newAnecdote= await anecdoteService.createNew(anecdote)
+    dispatch(createAnecdote(newAnecdote))
+    dispatch(setNotification(`Created a new anecdote: ${newAnecdote}`))
     setTimeout (() => {
         dispatch(removeNotification())
     }, 5000)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -58,14 +58,10 @@ export const voteAnecdote = (id) => {
   }
 }
 
-export const createAnecdote = (anecdote) => {
+export const createAnecdote = (data) => {
   return{
     type: 'NEW_ANECDOTE',
-    data: {
-      content: anecdote,
-      id: getId(),
-      votes: 0
-    }
+    data,
   }
 }
 
