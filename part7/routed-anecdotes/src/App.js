bf47068797a5a8d0ea11018f532da7e25c61af13
Obinna Okafor <okaforobinna7@gmail.com>
2021-05-19 19:19:47 +0100

    Commit for exercise 7.3

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Switch,
   Route,
   Link,
-  useRouteMatch
-} from 'react-router-dom'
+  useHistory,
+  useRouteMatch,
+} from "react-router-dom";
+
 
 const Menu = () => {
   const padding = {
@@ -19,16 +21,6 @@ const Menu = () => {
   )
 }
 
-const Anecdote = ({anecdote})  => {
-  return(
-    <div>
-      <h2>{anecdote.content}</h2>
-      <div>has {anecdote.votes} votes</div>
-      <div>for more info see {anecdote.info}</div>
-    </div>
-  )
-}
-
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
@@ -38,32 +30,49 @@ const AnecdoteList = ({ anecdotes }) => (
   </div>
 )
 
+
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
     <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an
+      incident. Occasionally humorous, anecdotes differ from jokes because their
+      primary purpose is not simply to provoke laughter but to reveal a truth
+      more general than the brief tale itself, such as to characterize a person
+      by delineating a specific quirk or trait, to communicate an abstract idea
+      about a person, place, or thing through the concrete details of a short
+      narrative. An anecdote is "a story with a point."
+    </em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    <p>
+      Software engineering is full of excellent anecdotes, at this app you can
+      find the best and add more.
+    </p>
   </div>
-)
+);
 
 const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
-
-    See <a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+  <div style={{ backgroundColor: "#eee" }}>
+    Anecdote app for{" "}
+    <a href="https://courses.helsinki.fi/fi/tkt21009">
+      Full Stack -websovelluskehitys
+    </a>
+    . See{" "}
+    <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
+      https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js
+    </a>{" "}
+    for the source code.
   </div>
-)
+);
 
 const CreateNew = (props) => {
   const [content, setContent] = useState('')
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
+
+  const history = useHistory()
 
 
   const handleSubmit = (e) => {
@@ -75,11 +84,11 @@ const CreateNew = (props) => {
       votes: 0
     })
     //console.log('content is ', content)
-    setContent('')
+    //setContent('')
     //console.log('content is ', content)
-    setAuthor('')
-    setInfo('')
-
+    ///setAuthor('')
+    //setInfo('')
+    history.push('/')
   }
 
   return (
@@ -104,6 +113,26 @@ const CreateNew = (props) => {
   )
 
 }
+const Anecdote = ({anecdote})  => {
+  return(
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>has {anecdote.votes} votes</div>
+      <div>for more info see {anecdote.info}</div>
+    </div>
+  )
+}
+
+const Notification = ({notification}) => {
+  console.log('Notification message is  ', notification)
+  return (
+    <div  >
+      { notification }
+    </div>
+  )
+}
+
+
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -127,14 +156,26 @@ const App = () => {
   const anecdote= match ? anecdotes.find(anecdote => Number(anecdote.id) === Number(match.params.id)) : null
   const [notification, setNotification] = useState('')
 
+  const notify = (message) => {
+    //console.log('notify message is ', message)
+    setNotification(message)
+    //const message1 = notification
+    //console.log('notification is', message1)
+    setTimeout(() => {
+      setNotification('')
+    }, 10000)
+  }
+
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
+    notify(`a new anecdote ${anecdote.content} created!`)
+    
   }
 
+  
   const anecdoteById = (id) =>
     anecdotes.find(a => a.id === id)
-
   const vote = (id) => {
     const anecdote = anecdoteById(id)
 
@@ -149,6 +190,7 @@ const App = () => {
   return(
     <div>
       <h1>Software anecdotes</h1>
+      <Notification notification = {notification} />
         <Menu />
         <Switch>
           <Route path= "/about">
@@ -168,5 +210,4 @@ const App = () => {
     </div>
   )
 }
-
 export default App;
